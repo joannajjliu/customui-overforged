1e0ef02d4654d4a97123701bdeee66e371f87cee
Joanna Liu <joanna.liu3774@gmail.com>
2023-01-03 23:59:19 -0500

    initial commit

---
+++ static/hello-world/src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+// import "@atlaskit/css-reset";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>HELLO</h1>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
