95f0c4b6e9d718a9b84ba32ef0f2a8d1c16f86e4
Joanna Liu <joanna.liu3774@gmail.com>
2023-01-06 00:06:43 -0500

    add api support

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import Resolver from "@forge/resolver";
+import api, { route } from "@forge/api";
 
 const resolver = new Resolver();
 
-resolver.define("getText", (req) => {
-  console.log(req);
+resolver.define("getText", async (req) => {
+  const response = await api.asApp().requestJira(route`/rest/api/3/search`);
+  const data = await response.json();
+  console.log("number of issues", data.total);
 
-  return "Hello, world! HIPPOS";
+  console.log(req);
+  console.log(`Project key ${req.context.extension.project.key}`);
+
+  return `Hello, world! HIPPOS ${req.context.extension.project.key}`;
+});
+
+resolver.define("getUsers", async (req) => {
+  // https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-users/#api-rest-api-3-users-search-get
+  const response = await api
+    .asApp()
+    .requestJira(route`/rest/api/3/users/search`);
+  const data = await response.json();
+
+  return "Hello, world!";
 });
 
 export const handler = resolver.getDefinitions();
--- static/hello-world/src/App.tsx
+++ static/hello-world/src/App.tsx
@@ -1,19 +1,19 @@
 // @ts-nocheck
 import React, { useEffect, useState, useRef, useCallback } from "react";
-// import { invoke } from "@forge/bridge";
+import { invoke } from "@forge/bridge";
 import Canvas from "./Canvas";
 
 function App() {
   const [data, setData] = useState(null);
 
-  //   useEffect(() => {
-  //     invoke("getText", { example: "my-invoke-variable" }).then(setData);
-  //   }, []);
+  useEffect(() => {
+    invoke("getText", { example: "my-invoke-variable" }).then(setData);
+  }, []);
 
   return (
     <div>
-      {/* {data ? data : "Give me a minute..."} */}
-      <Canvas />
+      {data ? data : "Give me a minute..."}
+      {/* <Canvas /> */}
     </div>
   );
 }
